5c9473d74dd7920b5f3000096ae20438ace834f2
OmerShahzadXoho <omers@xoho.tech>
2021-10-04 13:37:02 +0500

    -m

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import herderStyle from '../styles/Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={herderStyle.title}>
+            <h1><span>WebDev</span> News </h1>
+            <p className={herderStyle.description}> Keep up to date with ltest web dev news</p>
+        </div>
+    )
+}
+
+export default Header
---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/dist/shared/lib/head'
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About Page</title>
+            </Head>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default about
